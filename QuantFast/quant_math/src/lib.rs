052d8b977b3855aeef4abfdad00d4ef00ffab785
rafael <rafael.19.rosendo@gmail.com>
2026-04-29 12:58:56 -0300

    AIW-817: Criar, feito alterações da regra negocial para a task 817

--- QuantFast/quant_math/src/lib.rs
+++ QuantFast/quant_math/src/lib.rs
@@ -6,33 +6,15 @@ use rayon::prelude::*;
 use std::{f64::consts::{E}};
 use statrs::distribution;
 use statrs::distribution::{Continuous, ContinuousCDF};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 #[serde(rename_all = "snake_case")]
-
-pub struct OptionInputs {
-    pub s: f64,    // Spot price
-    pub k: f64,    // Strike price
-    pub r: f64,    // Risk-free rate
-    pub sig: f64,  // Volatility
-    pub t: f64,    // Time to maturity
-}
-
-pub struct GreeksOptions{
-    pub S: f64,
-    pub K: f64,
-    pub r: f64,
-    pub sig: f64,
-    pub T: f64,
-    pub t: f64,
-}
-
 pub struct FinanceData{
     // 
 }
 
-
+#[derive(Serialize, Deserialize, Debug)]
 pub enum FinanceMethod{
     CallEurope,
     PutEurope,
@@ -47,6 +29,18 @@ pub enum FinanceMethod{
     RhoPut,
     MonteCarlo,
     MonteCarloFast,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FinanceRequest {
+    pub to: String,
+    pub method: FinanceMethod,
+    pub s: f64,
+    pub k: f64,
+    pub r: f64,
+    pub sig: f64,
+    pub t_expiry: f64,
+    pub t_start: f64,
 }
 
 
@@ -268,25 +262,34 @@ pub fn monte_carlo_fast_f(
     (total_payoff / iterations as f64) * (-r * t).exp()
 }
 
-pub fn finance_factory(method: FinanceMethod, data: &GreeksOptions) -> f64{
-    let GreeksOptions {S, K, r, sig, T, t} = *data;
+pub fn finance_factory(method: FinanceMethod, data: FinanceRequest) -> f64{
+    let FinanceRequest {
+            s,
+            k,
+            r,
+            sig,
+            t_expiry,
+            t_start,
+            to,
+            method,
+        } = data;
 
-    let d1 = || d1(S, K, r, sig, T, t);
-    let d2 = || d2(S, K, r, sig, T, t);
+    let d1 = || d1(s, k, r, sig, t_expiry, t_start);
+    let d2 = || d2(s, k, r, sig, t_expiry, t_start);
 
     match method {
         FinanceMethod::CallDelta => call_delta_f(d1()),
         FinanceMethod::PutDelta => put_delta_f(d1()),
-        FinanceMethod::PutEurope => put_europe_f(S, K, r, sig, T, t),
-        FinanceMethod::CallEurope => call_europe_f(S, K, r, sig, T, t),
-        FinanceMethod::CallGamma => call_gamma_f(d1(), S, sig, T, t),
-        FinanceMethod::PutGamma => put_gamma_f(d1(), S, sig, T, t),
-        FinanceMethod::Vega => vega_f(S, d1(), T, t),
-        FinanceMethod::ThetaCall => theta_call_f(S, d1(), sig, T, t, r, K, d2()),
-        FinanceMethod::ThetaPut => theta_put_f(S, d1(), sig, T, t, r, K, d2()),
-        FinanceMethod::RhoCall => rho_call_f(K, T, t, r, d2()),
-        FinanceMethod::RhoPut => rho_put_f(K, T, t, r, d2()),
-        FinanceMethod::MonteCarlo => monte_carlo_f(S, r, sig, T, K, 1000, 10_000),
-        FinanceMethod::MonteCarloFast => monte_carlo_fast_f(S, r, sig, T, K, 1000, 10_000),
+        FinanceMethod::PutEurope => put_europe_f(s, k, r, sig, t_expiry, t_start),
+        FinanceMethod::CallEurope => call_europe_f(s, k, r, sig, t_expiry, t_start),
+        FinanceMethod::CallGamma => call_gamma_f(d1(), s, sig, t_expiry, t_start),
+        FinanceMethod::PutGamma => put_gamma_f(d1(), s, sig, t_expiry, t_start),
+        FinanceMethod::Vega => vega_f(s, d1(), t_expiry, t_start),
+        FinanceMethod::ThetaCall => theta_call_f(s, d1(), sig, t_expiry, t_start, r, k, d2()),
+        FinanceMethod::ThetaPut => theta_put_f(s, d1(), sig, t_expiry, t_start, r, k, d2()),
+        FinanceMethod::RhoCall => rho_call_f(k, t_expiry, t_start, r, d2()),
+        FinanceMethod::RhoPut => rho_put_f(k, t_expiry, t_start, r, d2()),
+        FinanceMethod::MonteCarlo => monte_carlo_f(s, r, sig, t_expiry, k, 1000, 10_000),
+        FinanceMethod::MonteCarloFast => monte_carlo_fast_f(s, r, sig, t_expiry, k, 1000, 10_000),
     }
 }
